739f79724bedd13f8cc934712c570783c8e514ef
dadaiwei <dadaiwei@lvwan.com>
2019-09-02 12:01:27 +0800

    :art:Update docs folder.

--- examples/tools/webpack.prod.config.js
+++ examples/tools/webpack.prod.config.js
@@ -1,8 +1,6 @@
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.com.config");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(baseConfig, {
-  mode: "production",
-  plugins: [new BundleAnalyzerPlugin()]
+  mode: "production"
 });
